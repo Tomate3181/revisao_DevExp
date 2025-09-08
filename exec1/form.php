4c4a30d485c45e32fc46c929ae371a63259031d0
felipenhoslol <felipenhos.1000@gmail.com>
2025-09-08 11:18:40 -0300

    exec1 sla

---
+++ exec1/form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+    <div class="sidenav">    
+        <div class="icon">🛒</div>
+        <a href="./cadastrar.php">Cadastrar</a>
+    </div>
+    <main style="margin-left: 140px;">
+        <h1>Loja ADM</h1>
+        <div class="imagem">
+        <img src="./1204143.jpg" alt="">
+        </div>
+    </main>
+</body>
+</html>
